502512c7f42e41ca3c1b35bc5e2399759253f0b3
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-03-04 19:36:04 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>InsightAI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/abdelmjid-saber/insightai-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: '',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/abdelmjid-saber/insightai-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'InsightAI Docs',
   },
 }
 
